3250d51a8f58efaf2804af26546b2b32834ed587
T Mark <tmk1.tran@gmail.com>
2023-11-07 17:23:23 -0600

    updated repo name to client-project-RD

--- server/server.js
+++ server/server.js
@@ -23,6 +23,7 @@ app.use(passport.session());
 
 /* Routes */
 app.use('/api/user', userRouter);
+// app.use('api/organizations', orgRouter);
 
 // Serve static files
 app.use(express.static('build'));
